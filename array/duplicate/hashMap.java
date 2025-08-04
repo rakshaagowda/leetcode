a7d781aa7161300cca38fae5501dc1750d1c0ace
RAKSHA B R <rakshagowdah264@gmail.com>
2025-08-04 22:48:20 +0530

    added solution using hashmap data structure

---
+++ array/duplicate/hashMap.java
@@ -0,0 +1,27 @@
+package leetcode.array.duplicate;
+
+import java.util.HashMap;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+      HashMap<Integer,Integer> seen = new HashMap<>();
+      for(int num : nums){
+        if(seen.containsKey(num) && seen.get(num)>=1)
+          return true;
+        seen.put(num,seen.getOrDefault(num,0)+1);
+      }
+      return false;
+    }
+}
+public class hashMap {
+  public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 3, 4, 5};
+        boolean result = solution.containsDuplicate(nums);
+        System.out.println("Contains duplicate: " + result);
+
+        int[] nums2 = {1, 2, 3, 4, 4};
+        result = solution.containsDuplicate(nums2);
+        System.out.println("Contains duplicate: " + result);
+  }  
+}
