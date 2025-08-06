21451b74d24af220d901c490299a196553e7883b
RAKSHA B R <rakshagowdah264@gmail.com>
2025-08-06 10:15:07 +0530

    added problem statement

--- array/groupanagram/ListofStrings.java
+++ array/groupanagram/ListofStrings.java
@@ -1,4 +1,5 @@
 package groupanagram;
+//Given an array of strings strs, group the anagrams together. You can return the answer in any order
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
