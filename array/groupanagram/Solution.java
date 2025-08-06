cea8ce29df5de1fc9445bd32a8f8f00d9c5a688b
RAKSHA B R <rakshagowdah264@gmail.com>
2025-08-06 10:17:53 +0530

    imported packages

--- array/groupanagram/Solution.java
+++ array/groupanagram/Solution.java
@@ -1,9 +1,12 @@
 package groupanagram;
 
-
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> ans = new HashMap<>();
+        HashMap<String, List<String>> ans = new HashMap<>();
 
         for (String s : strs) {
             int[] count = new int[26];
