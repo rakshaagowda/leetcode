07aa3734a845bda9dc78632095e515892ee7d5dc
RAKSHA B R <rakshagowdah264@gmail.com>
2025-08-06 10:07:56 +0530

    added solution package

---
+++ array/groupanagram/ListofStrings.java
@@ -0,0 +1,22 @@
+package groupanagram;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class ListofStrings {
+  public List<List<String>> groupAnagrams(String[] strs) {
+    HashMap<String, List<String>> ans = new HashMap<>();
+    for(String s: strs){
+      char[] chars=s.toCharArray();
+      Arrays.sort(chars);
+      String key=new String(chars);
+      if(!ans.containsKey(key)) {
+        ans.put(key,new ArrayList<>());
+      }
+      ans.get(key).add(s);
+      }
+
+      return new ArrayList<>(ans.values());
+    }
+  }
+
