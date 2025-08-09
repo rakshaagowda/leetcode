0da94d20e839ef6fcf2fa188edbddbc234ac88c1
RAKSHA B R <rakshagowdah264@gmail.com>
2025-08-09 16:00:50 +0530

    added solution class

---
+++ array/LongestConsecutive/Solution.java
@@ -0,0 +1,29 @@
+package LongestConsecutive;
+
+import java.util.HashSet;
+
+public class Solution {
+  
+    public int longestConsecutive(int[] nums) {
+        if(nums == null || nums.length==0)
+            return 0;
+        HashSet<Integer> set = new HashSet<>();
+        for(int num:nums){
+            set.add(num);
+        }
+        int longest=0;
+        for(int num:set){
+            if(!set.contains(num-1)){
+                int current=num;
+                int length=1;
+                while(set.contains(current+1)){
+                    current++;
+                    length++;
+                }
+                longest=Math.max(length,longest);
+         }
+        }
+        return longest;
+    }
+}
+
