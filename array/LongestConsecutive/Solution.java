1b68e5b6b259a8d80513dc25c450935b6e62dc1c
RAKSHA B R <rakshagowdah264@gmail.com>
2025-08-09 16:01:46 +0530

    added problem solution

--- array/LongestConsecutive/Solution.java
+++ array/LongestConsecutive/Solution.java
@@ -1,5 +1,7 @@
 package LongestConsecutive;
+//Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 
+//You must write an algorithm that runs in O(n) time.
 import java.util.HashSet;
 
 public class Solution {
