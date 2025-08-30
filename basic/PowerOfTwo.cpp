fc4d9bde63a4f153b0de42eb43a1de549a36bd1f
RAKSHA B R <rakshagowdah264@gmail.com>
2025-08-30 20:52:17 +0530

    added solution

--- basic/PowerOfTwo.cpp
+++ basic/PowerOfTwo.cpp
@@ -1,20 +1,13 @@
-#include <climits>
-//Given an integer n, return true if it is a power of two. Otherwise, return false.
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n <= 0) return false;
-        
-        int ans = 1;
-        for (int i = 0; i <= 30; i++) {
-            if (ans == n) {
+        int ans=1;
+        for(int i=0;i<=30;i++){
+            if(ans==n){
                 return true;
             }
-            // Check for potential overflow before multiplying
-            if (ans > INT_MAX / 2) {
-                break;
-            }
-            ans = ans * 2;
+            if(ans<INT_MAX/2)
+                ans=ans*2;
         }
         return false;
     }
