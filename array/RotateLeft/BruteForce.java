ba54a7a33c5af472e0a15c05b6474e40e35de0c4
RAKSHA B R <rakshagowdah264@gmail.com>
2025-08-31 12:23:33 +0530

    added bruteforce solution

---
+++ array/RotateLeft/BruteForce.java
@@ -0,0 +1,17 @@
+package RotateLeft;
+//Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+public class BruteForce {
+ public static void rotate(int[] nums, int k) {
+   int n=nums.length;
+   k=k%n;
+   int rotated[]=new int[n];
+   for(int i=0;i<n;i++){
+     rotated[(i+k)%n]=nums[i];
+   }
+   for(int i=0;i<n;i++){
+     nums[i]=rotated[i];
+   }
+ }
+}
