1328beff11000abb88f33684863b90de24c2daab
RAKSHA B R <rakshagowdah264@gmail.com>
2025-08-31 12:37:55 +0530

    added optimal  solution

---
+++ array/RotateLeft/reversing.java
@@ -0,0 +1,20 @@
+package RotateLeft;
+//Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+public class reversing {
+  public static void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+      int temp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = temp;
+      start++;
+      end--;
+    }
+
+  }
+  public static void rotate(int[] nums, int k) {
+    k = k % nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+  }
+}
