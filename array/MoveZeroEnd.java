8fd922a1e74b428f371664603971f22897e95512
RAKSHA B R <rakshagowdah264@gmail.com>
2025-09-02 16:03:22 +0530

    added problem statement

--- array/MoveZeroEnd.java
+++ array/MoveZeroEnd.java
@@ -1,3 +1,7 @@
+/*Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array */
+ 
 public class MoveZeroEnd {
   public void moveZeroes(int[] nums) {
     int j=0;
