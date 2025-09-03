5560abb8db3a25cd5ebd6f26fb65bee58d213735
RAKSHA B R <rakshagowdah264@gmail.com>
2025-09-03 14:35:10 +0530

    added problem statement

--- array/SingleElement.java
+++ array/SingleElement.java
@@ -1,3 +1,8 @@
+/*
+ * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+ */
 public class SingleElement {
   public void singleElement(int[] nums) {
     int ans = 0;
