20413b6bceeb1de5de849493f8b1f980d8c900ea
RAKSHA B R <rakshagowdah264@gmail.com>
2025-09-03 09:28:43 +0530

    added problem statement

--- array/MissingNumber/Sum.java
+++ array/MissingNumber/Sum.java
@@ -1,4 +1,5 @@
-package array.MissingNumber;
+package MissingNumber;
+//Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 public class Sum {
   public int missingNumber(int[] nums) {
     int n = nums.length;
