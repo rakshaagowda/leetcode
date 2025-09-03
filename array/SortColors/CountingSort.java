fdf0008b556a4f6cf3e225565a8f5f8b72e116a5
RAKSHA B R <rakshagowdah264@gmail.com>
2025-09-03 15:07:25 +0530

    added solution using counting sort

--- array/SortColors/CountingSort.java
+++ array/SortColors/CountingSort.java
@@ -1,5 +1,11 @@
 package SortColors;
+/*
+ * Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
 
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+ */
 public class CountingSort {
   public void countingsort(int[] nums) {
     int count0=0,count1=0;
