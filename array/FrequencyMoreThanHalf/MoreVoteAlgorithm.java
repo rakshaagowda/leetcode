df573fd7c107f24e13a419ecbdcfaf6c7d579942
RAKSHA B R <rakshagowdah264@gmail.com>
2025-09-04 10:36:18 +0530

    added solution explanation about  Booyer More's Vothing algorithm and problem statement

--- array/FrequencyMoreThanHalf/MoreVoteAlgorithm.java
+++ array/FrequencyMoreThanHalf/MoreVoteAlgorithm.java
@@ -1,5 +1,9 @@
 package FrequencyMoreThanHalf;
+/*
+ * Given an array nums of size n, return the majority element.
 
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+ */
 public class MoreVoteAlgorithm {
   public int majorityElement(int[] nums) {
     int count=0;
@@ -19,3 +23,16 @@ public class MoreVoteAlgorithm {
     return candidate;
   }
 }
+/*INTUITION
+ * The intuition behind the Moore's Voting Algorithm is based on the fact that if there is a majority element in an array, it will always remain in the lead, even after encountering other elements.
+
+Explanation:
+Algorithm:
+
+Initialize two variables: count and candidate. Set count to 0 and candidate to an arbitrary value.
+Iterate through the array nums:
+a. If count is 0, assign the current element as the new candidate and increment count by 1.
+b. If the current element is the same as the candidate, increment count by 1.
+c. If the current element is different from the candidate, decrement count by 1.
+After the iteration, the candidate variable will hold the majority element.
+ */
