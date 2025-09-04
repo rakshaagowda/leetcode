d0e37ed2b796195bcb60156df2251dab9ae0d9c5
RAKSHA B R <rakshagowdah264@gmail.com>
2025-09-04 10:20:46 +0530

    added problem statement

--- array/FrequencyMoreThanHalf/UsingHashMap.java
+++ array/FrequencyMoreThanHalf/UsingHashMap.java
@@ -2,6 +2,11 @@ package FrequencyMoreThanHalf;
 
 import java.util.HashMap;
 import java.util.Map;
+/*
+ * Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+ */
 
 public class UsingHashMap {
    public int majorityElement(int[] nums) {
