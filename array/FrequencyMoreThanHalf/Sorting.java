e9b56d3e1aaaa35b1f59015b8926910d984b8895
RAKSHA B R <rakshagowdah264@gmail.com>
2025-09-04 10:20:07 +0530

    added solution using HashMap

--- array/FrequencyMoreThanHalf/Sorting.java
+++ array/FrequencyMoreThanHalf/Sorting.java
@@ -1,5 +1,10 @@
 package FrequencyMoreThanHalf;
 import java.util.*;
+/*
+ * Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+ */
 public class Sorting {
   public int frequency(int[] nums) {
     int n=nums.length;
