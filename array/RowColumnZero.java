222d67bae5971ccd2131663de20a3da60983db9f
RAKSHA B R <rakshagowdah264@gmail.com>
2025-09-05 23:05:18 +0530

    added problem statement

--- array/RowColumnZero.java
+++ array/RowColumnZero.java
@@ -1,3 +1,8 @@
+/*
+ * Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+ */
 public class RowColumnZero {
   public void setZeroes(int[][] matrix) {
     int m = matrix.length, n = matrix[0].length;
