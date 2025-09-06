5a722a56a1add704b13eb40bdda6b9c81005e9b3
RAKSHA B R <rakshagowdah264@gmail.com>
2025-09-06 17:03:01 +0530

    added problem statement and solution approach

--- array/RotateMatrix.java
+++ array/RotateMatrix.java
@@ -1,3 +1,6 @@
+/*You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation. */
 public class RotateMatrix {
   public void rotate(int[][] matrix) {
     int n = matrix.length;
@@ -23,3 +26,8 @@ public class RotateMatrix {
     }
   }
 }
+/*
+ * To rotate a square matrix 90 degrees clockwise in-place, we can use a two-step transformation. First, we transpose the matrix by swapping elements across the main diagonal. Then, we reverse each row. This sequence of operations efficiently moves each element to its correct final position.
+
+
+ */
