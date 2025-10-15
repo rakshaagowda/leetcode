764dca9c50ad0ab16cde65fd7392b588ba12491e
RAKSHA B R <rakshagowdah264@gmail.com>
2025-10-15 19:55:06 +0530

    added solution

---
+++ array/Pascaltriangle.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+        for(int rownum=0; rownum<numRows; ++rownum){
+            vector<int> row;
+            row.push_back(1);
+            if(rownum > 0){
+                vector<int>& prevrow = triangle.back();
+                for(int j=1; j<rownum; ++j){
+                    row.push_back(prevrow[j-1] + prevrow[j]);
+                }
+                row.push_back(1);
+            }
+            triangle.push_back(row);
+        }
+        return triangle;
+    }
+};
